f74f0eacb02c4d3f6ce0c7a729d4d87d2cac6e51
DavidtheDJ <DavidtheDJ00@gmail.com>
2016-10-13 00:04:27 -0700

    added insert and delete

--- Linked-Lists.py
+++ Linked-Lists.py
@@ -13,27 +13,26 @@ class Node:
     def __str__(self):
         return str(self.contents)
 
-    def insert(self,data):
-        new_node = Node(data)
-        new_node.set_next(self.contents)
-        self.contents = new_node
-    
-    def delete(self,data):
-        current = self.contents
-        previous = None
-        found = False
-        while current and found is False:
-            if current.get_data() == data:
-                found = True
-            else:
-                previous = currnet
-                current = current.get_next()
-        if current is None:
-            raise ValueError("Data not in list")
-        if previous is None:
-            self.content = current.get_next()
+    def insert(self,node):
+        node.next = self.next  
+        self.next = node
+
+    def delete_node(node, contents):
+        PreviousNode = node
+
+        if node.contents == contents:
+            node.contents = "blank"
+            node = node.next
         else:
-            previous.set_next(surrent.get_next())
+            while node:            
+                if node.contents == contents:
+                    PreviousNode.next = node.next               
+
+                PreviousNode = node
+                node = node.next
+            
+    
+        
 
 def print_list(node):
     while node:
@@ -45,11 +44,11 @@ def testList():
     node1 = Node("car")
     node2 = Node("bus")
     node3 = Node("lorry")
-    node4 = Node("train")
-    #added = Node.insert("train")
     node1.next = node2
     node2.next = node3
-    node3.next = node4
+    node2.insert(Node("bike"))
+    node1.insert(Node("skateboard"))
+    Node.delete_node(node1, "car")
     print_list(node1)
 
 testList()
