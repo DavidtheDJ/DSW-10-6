fc67af789588910f46c1d6b253f8a22cfc8a2eab
DavidtheDJ <DavidtheDJ00@gmail.com>
2016-10-12 19:52:38 -0700

    Upload 2

--- Queues.py
+++ Queues.py
@@ -1,3 +1,35 @@
 #David Justice
 #10-6-19
 #Queues
+
+
+
+class Queue:
+    def __init__(self):
+        self.items=[]
+    def add(self,item):
+        self.items.append(item)
+    def delete(self):
+        itemToDelete = self.items[0]
+        del self.items[0]
+        return itemToDelete
+    def size(self):
+        return len(self.items)
+    def report(self):
+        return self.items
+
+
+
+def start():
+
+    givenQueue=Queue()
+    givenQueue.add("Bob")
+    givenQueue.add("Brodie")
+    givenQueue.add("Carrie")
+    givenQueue.add("Tanya")
+    print(givenQueue.size())
+    print(givenQueue.report())
+    print(givenQueue.delete())
+    print(givenQueue.report())
+
+start()
